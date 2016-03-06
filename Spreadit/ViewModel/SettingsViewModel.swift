7e834eddeb30610e0b384de302b4734274ee5a4e
Marko Hlebar <marko.hlebar@gmail.com>
2016-03-06 17:27:39 +0000

    Added ability to change app store URL on the fly.

--- Spreadit/ViewModel/SettingsViewModel.swift
+++ Spreadit/ViewModel/SettingsViewModel.swift
@@ -9,7 +9,7 @@
 import UIKit
 import BIND
 
-let appStoreURL = "https://itunes.apple.com/us/app/fontanakey/id1069595637?ls=1&mt=8"
+let kAppStoreURL = "https://itunes.apple.com/us/app/fontanakey/id1069595637?ls=1&mt=8"
 
 class SettingsViewModel: BNDViewModel, UIActivityItemSource {
 
@@ -19,12 +19,12 @@ class SettingsViewModel: BNDViewModel, UIActivityItemSource {
     
     func activityViewController(activityViewController: UIActivityViewController, itemForActivityType activityType: String) -> AnyObject? {
         
-        let url = NSURL(string: appStoreURL)!
-        let message = "FontanaKey lets you search for URL-s without leaving the chat!\n\(url.absoluteString)"
+        let url = NSURL(string: appStoreURL())!
+        let message = "Fontana lets you search for links without leaving the chat!\n\(url.absoluteString)"
         
         switch (activityType) {
         case UIActivityTypePostToTwitter:
-            return "@fontanakey lets you search for URL-s without leaving the chat!\n\(url.absoluteString)"
+            return "@fontanakey lets you search for links without leaving the chat!\n\(url.absoluteString)"
         case UIActivityTypePostToFacebook,
              UIActivityTypeCopyToPasteboard:
             return url
@@ -33,7 +33,14 @@ class SettingsViewModel: BNDViewModel, UIActivityItemSource {
         }
     }
     
+    func appStoreURL() -> String {
+        if let urlString = FNTAppTracker.variableForKey("appStoreURL") as? String {
+            return urlString
+        }
+        return kAppStoreURL
+    }
+    
     func activityViewController(activityViewController: UIActivityViewController, subjectForActivityType activityType: String?) -> String {
-        return "Check out FontanaKey"
+        return "Check Out Fontana"
     }
 }
